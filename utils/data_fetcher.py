0a733fef07e1374405a207aa18083808fb3959d6
xavnet82 <x.lopezc@gmail.com>
2025-06-04 15:37:30 +0200

    Update data_fetcher.py

--- utils/data_fetcher.py
+++ utils/data_fetcher.py
@@ -1,13 +1,36 @@
-
 import yfinance as yf
 import pandas as pd
 
-def descargar_datos(ticker):
-    df = yf.download(ticker, period="1y")
-    return df
-
 def get_sp500_tickers():
-    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    table = pd.read_html(url)
-    df = table[0]
-    return df['Symbol'].sort_values().tolist()
+    # Lista simplificada o puedes parsear desde Wikipedia
+    return [
+        "AAPL", "AC", "MSFT", "GOOGL", "AMZN", "META", "NVDA", "JPM", "JNJ", "V", "PG"
+    ]
+
+def get_nasdaq100_tickers():
+    return [
+        "AAPL", "MSFT", "AMZN", "NVDA", "GOOG", "TSLA", "META", "PEP", "AVGO", "COST"
+    ]
+
+def get_eurostoxx50_tickers():
+    return [
+        "ADS.DE", "AIR.PA", "ALV.DE", "BNP.PA", "DAI.DE", "ENEL.MI", "IBE.MC", "OR.PA", "SAN.MC", "SAP.DE"
+    ]
+
+def get_ibex35_tickers():
+    return [
+        "SAN.MC", "BBVA.MC", "ITX.MC", "TEF.MC", "REP.MC", "AENA.MC", "ACS.MC", "GRF.MC"
+    ]
+
+def get_nasdaq_tickers():
+    # Lista ejemplo – idealmente sustituible por parsing o fuente externa
+    return [
+        "AAPL", "TSLA", "NVDA", "AMD", "INTC", "PYPL", "ADBE", "NFLX"
+    ]
+
+def descargar_datos(ticker, periodo="1y", intervalo="1d"):
+    try:
+        df = yf.download(ticker, period=periodo, interval=intervalo, progress=False)
+        return df
+    except Exception as e:
+        return pd.DataFrame()
