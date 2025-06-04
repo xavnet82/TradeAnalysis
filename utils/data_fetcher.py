3e666e7e4dcf3c3762d3d7f297ee3bbc9022a4c9
xavnet82 <x.lopezc@gmail.com>
2025-06-04 11:41:38 +0200

    Update data_fetcher.py

--- utils/data_fetcher.py
+++ utils/data_fetcher.py
@@ -1,6 +1,12 @@
-
 import yfinance as yf
+import pandas as pd
 
 def descargar_datos(ticker):
     df = yf.download(ticker, period="1y")
     return df
+
+def get_sp500_tickers():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    table = pd.read_html(url)
+    df = table[0]
+    return df['Symbol'].sort_values().tolist()
